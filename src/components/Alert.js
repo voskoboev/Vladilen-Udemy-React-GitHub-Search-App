7bc89db88d08335e077c28a356e004443b870ae2
Dmitry <dvoskoboev@gmail.com>
2021-08-19 19:51:52 +0300

    Added alert and github state.

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
-const Alert = ({ alert }) => {
+const Alert = () => {
+  const { alert, hide } = useContext(AlertContext)
+
+  if (!alert) return null
+
   return (
     <div className={`alert alert-${alert.type || 'secondary'} alert-dismissible`} role="alert">
       {alert.text}
-      <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={hide}
+      ></button>
     </div>
   )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
 
 const Search = () => {
+  const { show } = useContext(AlertContext)
+  const [value, setValue] = useState('')
+
+  const onSubmit = ev => {
+    if (ev.key !== 'Enter') {
+      return
+    }
+
+    if (value.trim()) {
+      console.log('request ', value)
+    } else {
+      show('enter users data!')
+    }
+  }
+
   return (
     <div className="form-gruop mb-3">
-      <input 
+      <input
         type="text"
         className="form-control"
         placeholder="Enter user name"
-
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={ev => setValue(ev.target.value)}
       />
     </div>
   )
